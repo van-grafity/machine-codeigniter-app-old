457184d1f63de1f07d3162b0a53867566badcae3
PKL (GLA) <friscafebrianti16@gmail.com>
2023-08-28 14:44:29 +0700

    add data table

--- app/Views/brand/index.php
+++ app/Views/brand/index.php
@@ -30,7 +30,7 @@
                         </div>
                         <!-- /.card-header -->
                         <div class="card-body">
-                            <table id="brand_table" class="table table-bordered table-hover">
+                            <table id="brand_table" class="table table-bordered table-hover masterdata-datatable">
                                 <thead>
                                     <tr>
                                         <th>No</th>
--- app/Views/machine-type/index.php
+++ app/Views/machine-type/index.php
@@ -30,7 +30,7 @@
                         </div>
                         <!-- /.card-header -->
                         <div class="card-body">
-                            <table id="machine_type_table" class="table table-bordered table-hover">
+                            <table id="machine_type_table" class="table table-bordered table-hover masterdata-datatable">
                                 <thead>
                                     <tr>
                                         <th>No</th>
